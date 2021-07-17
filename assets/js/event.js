b1a174d805a8fdf2805ad79420f51f718766716e
nhatlebka <84030512+nhatlebka@users.noreply.github.com>
2021-07-17 17:42:18 +0700

    axax

---
+++ assets/js/event.js
@@ -0,0 +1,8 @@
+
+
+    // var canvas = document.getElementById('myCanvas'),
+    // ctx = canvas.getContext('2d');
+    // ctx.strokeStyle = "#FF0000";
+    // ctx.beginPath();
+    // ctx.arc(95,50,40,0,2*Math.PI);
+    // ctx.stroke();
